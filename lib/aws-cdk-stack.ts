2aa2a1b0de575eb9922a55454b4d2d999849ff8b
Vamaa-shah <vamashah.1123@gmail.com>
2025-03-23 10:39:05 -0400

    Initial commit

---
+++ lib/aws-cdk-stack.ts
@@ -0,0 +1,46 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+ 
+export class AwsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+ 
+    // Create an S3 Bucket
+    const myBucket = new s3.Bucket(this, 'vshah3026-MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Only for dev/test environments
+    });
+ 
+    // Create a Lambda Function
+    const myLambda = new lambda.Function(this, 'vshah3026-MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('Lambda invoked!');
+          return { statusCode: 200, body: 'Hello world!' };
+        }
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName,
+      },
+    });
+ 
+    // Grant Lambda permissions to read/write to the S3 bucket
+    myBucket.grantReadWrite(myLambda);
+ 
+    // Create a DynamoDB Table
+    const myTable = new dynamodb.Table(this, 'vshah3026-MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'vshah3026-MyTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,  // Only for dev/test environments
+    });
+ 
+    // Grant Lambda permissions to access DynamoDB
+    myTable.grantReadWriteData(myLambda);
+  }
+}
+ 
